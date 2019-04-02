f67e76e55d3c7c18e6160a0646edbd37f81aeb4e
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2019-04-02 18:23:08 +0100

    Added support for turning the computer on/off through the mobile application

--- app/src/main/java/com/example/vladislavsvasiljevs/pchub/HomeActivity.java
+++ app/src/main/java/com/example/vladislavsvasiljevs/pchub/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.vladislavsvasiljevs.pchub;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.content.Intent;
@@ -14,18 +17,51 @@ import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.time.OffsetDateTime;
+
+
 public class HomeActivity extends AppCompatActivity {
     //GridLayout mainGrid;
+
+    private DatabaseReference databaseReference;//Firebase reference
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
+        offButtonGONE();//By default the turn off button is set to GONE, When On button clicked, then this button will appear
 
         goToComputerStats();//Calling method to bring us to Computer Stat screen
         goToLightControl();//Calling method to bring us to Light control screen
         goToFanControl();//Calling method to bring us to Fan control screen
         goToDashboard();//Calling method to bring us to Dashboard screen
+        computerOnButton();//Calling method to trigger button
+        computerOffButton();//Calling method to trigger button
+    }
+
+    private void computerOnButton() {//Method that turns on the computer
+        Button computerOn = findViewById(R.id.greenBtn);//Assigning button to computerOn
+        computerOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialogOnButton();//Calling alert dialog method for on button
+            }
+        });
+    }
+
+    private void computerOffButton() {//Method that turns off the computer
+        Button computerOff = findViewById(R.id.redBtn);//Assigning a button to computer off
+        computerOff.setOnClickListener(new View.OnClickListener() {//Creating a click listener
+            @Override
+            public void onClick(View view) {
+                alertDialogOffButton();//Calling alert dialog method for on button
+            }
+        });
     }
 
     private void goToComputerStats() {//Method that brings us to Computer stat screen
@@ -82,5 +118,83 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    private void offButtonGONE() {
+        Button computerOff = findViewById(R.id.redBtn);
+        computerOff.setVisibility(View.GONE);
+    }
+
+    private void offButtonVisible() {
+        Button computerOff = findViewById(R.id.redBtn);
+        computerOff.setVisibility(View.VISIBLE);
+    }
+
+    private void onButtonVisible() {
+        Button computerOn = findViewById(R.id.greenBtn);
+        computerOn.setVisibility(View.VISIBLE);
+    }
+
+    private void onButtonGONE() {
+        Button computerOn = findViewById(R.id.greenBtn);
+        computerOn.setVisibility(View.GONE);
+    }
+
+//This snippet has came from the following webiste: https://www.tutorialspoint.com/how-do-i-display-an-alert-dialog-on-android
+//    Some parts where edited by me (Vladislavs Vasiljes) to match my projects requirements
+    private void alertDialogOnButton() {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setMessage("If your computer is already turned on and you wish to turn it off then tap Continue. " +
+                "If your computer is turned off but you wish to turn it on then tap Continue. Otherwise tap Cancel.");
+        dialog.setTitle("Warning!");
+        dialog.setPositiveButton("Continue",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+                        Toast.makeText(getApplicationContext(),"Yes is clicked",Toast.LENGTH_LONG).show();
+                        databaseReference.child("computerControl").setValue(1);
+                        onButtonGONE();
+                        offButtonVisible();
+                    }
+                });
+        dialog.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"cancel is clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+        AlertDialog alertDialog=dialog.create();
+        alertDialog.show();
+    }
+
+    //This snippet has came from the following webiste: https://www.tutorialspoint.com/how-do-i-display-an-alert-dialog-on-android
+//    Some parts where edited by me (Vladislavs Vasiljes) to match my projects requirements
+    private void alertDialogOffButton() {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setMessage("If your computer is already turned on and you wish to turn it off then tap Continue. " +
+                "If your computer is turned off but you wish to turn it on then tap Continue. Otherwise tap Cancel.");
+        dialog.setTitle("Warning!");
+        dialog.setPositiveButton("Continue",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+                        Toast.makeText(getApplicationContext(),"Yes is clicked",Toast.LENGTH_LONG).show();
+                        databaseReference.child("computerControl").setValue(1);
+                        onButtonVisible();
+                        offButtonGONE();
+                    }
+                });
+        dialog.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"cancel is clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+        AlertDialog alertDialog=dialog.create();
+        alertDialog.show();
     }
 }
+
+
+
