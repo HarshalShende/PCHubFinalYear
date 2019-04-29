97997790a5bce76c7decffd74a3f93eedbbd2242
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2019-04-29 21:49:28 +0100

    Added settings to the menu action bar Now we can change the limit on when to send a notification in terms of cpu,gpu time and how often should we check for the value limit

--- app/src/main/java/com/example/vladislavsvasiljevs/pchub/Models/settings.java
+++ app/src/main/java/com/example/vladislavsvasiljevs/pchub/Models/settings.java
@@ -1,4 +1,33 @@
 package com.example.vladislavsvasiljevs.pchub.Models;
 
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@IgnoreExtraProperties
 public class settings {
+    public String CPUTempAlert;
+    public String GPUTempAlert;
+    public int FrequencyOfNotifications;
+
+    public settings() {
+    }
+
+    public settings(String cPUTempAlert, String gPUTempAlert,int frequencyOfNotifications) {
+        this.CPUTempAlert = cPUTempAlert;
+        this.GPUTempAlert = gPUTempAlert;
+        this.FrequencyOfNotifications = frequencyOfNotifications;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("Value", CPUTempAlert);
+        result.put("Max", GPUTempAlert);
+        result.put("Max", FrequencyOfNotifications);
+
+        return result;
+    }
 }
--- app/src/main/java/com/example/vladislavsvasiljevs/pchub/settingsActivity.java
+++ app/src/main/java/com/example/vladislavsvasiljevs/pchub/settingsActivity.java
@@ -2,12 +2,155 @@ package com.example.vladislavsvasiljevs.pchub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.vladislavsvasiljevs.pchub.Models.settings;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class settingsActivity extends AppCompatActivity {
+
+    TextView getcpuTempLimit, getgpuTempLimit,getNotificationLimit;
+    EditText setCPULimit,setGPULimit,setNotificationLimit;
+    private DatabaseReference limitReference, limit;
+    private ValueEventListener limitListener;
+    Button updateValue,UpdateValue2,UpdateValue3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        getcpuTempLimit = findViewById(R.id.getCPULimit);
+        getgpuTempLimit = findViewById(R.id.getGPULimit);
+        getNotificationLimit = findViewById(R.id.getNotificationTime);
+        setCPULimit = findViewById(R.id.setCPULimit);
+        setGPULimit = findViewById(R.id.setGPULimit);
+        setNotificationLimit = findViewById(R.id.setNotificationLimit);
+
+        limitReference = FirebaseDatabase.getInstance().getReference("EmailNotification");//Link to CPU temps readings
+        limit = FirebaseDatabase.getInstance().getReference();//Link to CPU temps readings
+        CPUAlert();
+        GPUAlert();
+        NotificationTimerAlert();
+        deleteTextOnClick();
+        deleteTextOnClick2();
+        deleteTextOnClick3();
+
+    }
+
+    private void CPUAlert() {//Method that turns on the computer
+        updateValue = findViewById(R.id.updateValue);
+        updateValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String setCPULimit = settingsActivity.this.setCPULimit.getText().toString();
+                limit.child("EmailNotification/CPUTempAlert").setValue(setCPULimit+" °C");
+
+            }
+        });
+}
+
+    private void GPUAlert() {//Method that turns on the computer
+        UpdateValue2 = findViewById(R.id.UpdateValue2);
+        UpdateValue2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String setGPULimit = settingsActivity.this.setGPULimit.getText().toString();
+                limit.child("EmailNotification/GPUTempAlert").setValue(setGPULimit+" °C");
+            }
+        });
+    }
+
+    private void NotificationTimerAlert() {//Method that turns on the computer
+        UpdateValue3 = findViewById(R.id.UpdateValue3);
+        UpdateValue3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String setNotificationLimit  = settingsActivity.this.setNotificationLimit.getText().toString();
+                int value;
+                value = Integer.parseInt(setNotificationLimit);
+                limit.child("EmailNotification/FrequencyOfNotifications").setValue(value);
+            }
+        });
+    }
+
+
+    private void deleteTextOnClick() {//Method that turns on the computer
+        setCPULimit = findViewById(R.id.setCPULimit);
+        setCPULimit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setCPULimit.setText("");
+
+            }
+        });
+    }
+
+    private void deleteTextOnClick2() {//Method that turns on the computer
+        setGPULimit = findViewById(R.id.setGPULimit);
+        setGPULimit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setGPULimit.setText("");
+
+            }
+        });
+    }
+
+    private void deleteTextOnClick3() {//Method that turns on the computer
+        setNotificationLimit = findViewById(R.id.setNotificationLimit);
+        setNotificationLimit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setNotificationLimit.setText("");
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //CPU Temp Readings from FireBase
+        ValueEventListener limitListener = new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    settings settings = dataSnapshot.getValue(settings.class);
+
+                    // Log.e(TAG, "onDataChange: Message data is updated: " + cpuTempReading.Value + ", " + ", ");
+
+                    getcpuTempLimit.setText("Current Processor Temperature limit: "+settings.CPUTempAlert);
+                    getgpuTempLimit.setText("Current Graphics Processor Temperature limit: "+settings.GPUTempAlert);
+                    getNotificationLimit.setText("Current wait until checking for limits in seconds: "+settings.FrequencyOfNotifications);
+                    setCPULimit.setText(settings.CPUTempAlert);
+                    setGPULimit.setText(settings.GPUTempAlert);
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                getcpuTempLimit.setText("");
+                getgpuTempLimit.setText("");
+                getNotificationLimit.setText("");
+            }
+        };
+
+        limitReference.addValueEventListener(limitListener);
+
+        // copy for removing at onStop()
+        this.limitListener = limitListener;
     }
 }
